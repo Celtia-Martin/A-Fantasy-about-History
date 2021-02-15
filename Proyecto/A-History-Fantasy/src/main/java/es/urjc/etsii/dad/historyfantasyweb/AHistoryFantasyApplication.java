a66f309f7176b9e04235d9d205b3aef6f922ead4
Celtia <c.martinga.2017@alumnos.urjc.es>
2021-02-15 13:58:34 +0100

    Vuelvo a subir el proyecto
    Estamos teniendo problemas tecnicos

---
+++ Proyecto/A-History-Fantasy/src/main/java/es/urjc/etsii/dad/historyfantasyweb/AHistoryFantasyApplication.java
@@ -0,0 +1,13 @@
+package es.urjc.etsii.dad.historyfantasyweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AHistoryFantasyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AHistoryFantasyApplication.class, args);
+	}
+
+}
---
+++ Proyecto/A-History-Fantasy/src/main/java/es/urjc/etsii/dad/historyfantasyweb/User.java
@@ -0,0 +1,9 @@
+package es.urjc.etsii.dad.historyfantasyweb;
+
+import org.springframework.web.context.annotation.SessionScope;
+
+
+//@Entity
+public class User {
+
+}
